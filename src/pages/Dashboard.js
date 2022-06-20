f4653f9ac03f5fd950fef87af28828aea9cab20d
Wilson Dos Santos <wilson-senx@hotmail.com>
2022-06-19 23:26:43 -0300

    :sparkles: Make style of my dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+
+//import logo from '../assets/logo.png';
 
 export default function Dashboard() {
   return (
-    <View>
-      <Text>Seja bem vindo</Text>
+    <View style={styles.container}>
+      {/*<Image style={styles.logo} source={logo} />*/}
+      <Text style={styles.logoText}>Seja bem vindo</Text>
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#413D4F'
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    marginTop: 10
+  },
+
+  logoText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    borderBottomWidth: 1,
+    borderBottomColor: '#666',
+    marginTop: 40,
+    color: '#eee',
+  }
+})
