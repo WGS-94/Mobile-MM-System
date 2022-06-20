dc3c71dcb02b070b64c74838e342971d665c6915
Wilson Dos Santos <wilson-senx@hotmail.com>
2022-06-20 02:02:14 -0300

    :bug: Solving the problem of push data from api

---
+++ src/components/MachineList.js
@@ -0,0 +1,88 @@
+import React from 'react';
+
+import { View, Text, FlatList, Image, StyleSheet} from 'react-native';
+
+export default function MachineList() {
+
+  const data = [
+    {
+      'id': 1,
+      'thumbnail': 'https://img.directindustry.com/pt/images_di/photo-mg/115707-15139071.jpg',
+      'name': 'Engel',
+      'parameter': '1000 kN',
+      'manufactur': 'Wilson Motores',
+      'descripion': 'Texto alternativo'
+    },
+    {
+      'id': 2,
+      'thumbnail': 'https://img.directindustry.com/pt/images_di/photo-mg/115707-15139071.jpg',
+      'name': 'Engel',
+      'parameter': '1000 kN',
+      'manufactur': 'Wilson Motores',
+      'descripion': 'Texto alternativo'
+    },
+    {
+      'id': 3,
+      'thumbnail': 'https://img.directindustry.com/pt/images_di/photo-mg/115707-15139071.jpg',
+      'name': 'Engel',
+      'parameter': '1000 kN',
+      'manufactur': 'Wilson Motores',
+      'descripion': 'Texto alternativo'
+    }
+]
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Sistem de Monitoramento de Máquinas</Text>
+      <FlatList 
+        style={styles.list}
+        data={data}
+        keyExtractor={f => f.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <View styel={styles.listItem}>
+            <Image styel={styles.thumbnail} source={{ uri: item.thumbnail }} />
+            <Text styel={styles.textItem}>Nome: {item.name}</Text>
+            <Text styel={styles.textItem}>Parâmetros: {item.parameter}</Text>
+            <Text styel={styles.textItem}>Fabricante: {item.manufactur}</Text>
+            <Text styel={styles.textItem}>Descrição: {item.descripion}</Text>
+          </View>
+        )}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#413D4F',
+    marginTop: 20
+  },
+
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#eee',
+    textAlign: 'center',
+    marginBottom: 20
+  },
+
+  list: {
+    paddingHorizontal: 20,
+  },
+  
+  listItem: {
+    marginRight: 15,
+  },
+
+  thumbnail: {
+    width: 200,
+    height: 120,
+    resizeMode: 'cover',
+    borderRadius: 3,
+  }
+
+
+})
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, SafeAreaView, Text, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import api from '../services/api';
+
+import MachineList from '../components/MachineList';
 
 //import logo from '../assets/logo.png';
 
 export default function Dashboard() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function loadData() {
+      /*const user_id = AsyncStorage.getItem("@mmsystem:userID")
+      console.log(user_id)
+      const response = await api.get(`/machines/user/${user_id}`);
+      const response = await api.get('/machines');
+      console.log(response.data.machines)
+      setData(response.data.machines);*/
+    }
+    loadData();
+  }, []);
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       {/*<Image style={styles.logo} source={logo} />*/}
-      <Text style={styles.logoText}>Seja bem vindo</Text>
-    </View>
+      <Text style={styles.logoText}>Dashboard</Text>
+        <MachineList />
+        {/*data.map(machine => <MachineList key={machine} machine={machine} />)*/}
+    </SafeAreaView>
   )
 }
 
@@ -19,19 +41,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#413D4F'
   },
 
-  logo: {
+  /*logo: {
     height: 32,
     resizeMode: 'contain',
     alignSelf: 'center',
     marginTop: 10
-  },
+  },*/
 
   logoText: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: 'bold',
+    marginTop: 20,
+    color: '#E8643A',
+    padding: 10,
+    textAlign: 'center',
     borderBottomWidth: 1,
     borderBottomColor: '#666',
-    marginTop: 40,
-    color: '#eee',
   }
 })
