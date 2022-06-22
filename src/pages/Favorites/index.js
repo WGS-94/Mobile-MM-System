15dc8e7f5821a092789e16daf75cdb10e8f4556e
Wilson Dos Santos <wilson-senx@hotmail.com>
2022-06-22 06:45:17 -0300

    :sparkles: create some folders

---
+++ src/pages/Favorites/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Container } from './style';
+
+export default function Favorites() {
+  return (
+    <Container>
+      <Text>Favorites</Text>
+    </Container>
+  );
+}
